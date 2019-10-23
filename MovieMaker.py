a900daf2f2ae089b017ec9d79ca296f9bce39930
LukasDSauer <lukasdsauer@yahoo.de>
2019-10-23 17:05:55 +0200

    restructuring folders

--- MovieMaker.py
+++
@@ -1,7 +0,0 @@
-#  Developed by Lukas Sauer at the Heidelberg Institute of Theoretical Studies on 3/29/19, 10:35 AM.
-#  Contact: lukas.sauer@h-its.org.
-#  Last modified on 3/29/19, 10:35 AM.
-#  (C) 2019. All rights reserved.
-
-
-
