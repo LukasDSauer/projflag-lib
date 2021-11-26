8f21d7e2dcbeba4516ba761941a4b457efa73b4a
Lukas D. Sauer <lukasdsauer@yahoo.de>
2021-11-26 16:04:44 +0100

    Update setup.py

--- projflag-lib/flagcomplex/setup.py
+++ projflag-lib/flagcomplex/setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flagcomplex",
-    version="2.3.1",
+    version="2.3.2",
     author="Lukas D. Sauer",
     # author_email="author@example.com",
     description="This is a project to perform transformations on flags in the real projective plane."
